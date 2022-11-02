053875d083dbc1acd90967539ec4df6ce0874db2
Zandarh <ubaksxander@gmail.com>
2022-11-02 09:26:09 +0100

    Update do_create

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -17,7 +24,9 @@ class FileStorage:
                 if key.startswith(cls.__name__):
                     new_obj[key] = val
             return new_obj
-        return FileStorage.__objects
+
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
